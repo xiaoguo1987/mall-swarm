e997ace62f97ca17e2b11041b9097085a4ce0b70
jianming00733@163.com <4658.jian>
2020-07-09 22:07:34 +0800

    注册中心，配置中心，网关修复，admin 后端配置修复，这样可以只用这几个中间件

--- mall-admin/src/main/java/com/macro/mall/config/GlobalCorsConfig.java
+++ mall-admin/src/main/java/com/macro/mall/config/GlobalCorsConfig.java
@@ -17,7 +17,7 @@ public class GlobalCorsConfig {
     /**
      * 允许跨域调用的过滤器
      */
-    @Bean
+   // @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
         //允许所有域名进行跨域调用
